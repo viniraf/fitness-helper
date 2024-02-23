a473f018acb20c30f307129ea8fefebc7822639d
viniraf <vrmb.pro@gmail.com>
2024-02-22 22:32:49 -0300

    Created enums

---
+++ FitnessHelper/Enums/ExerciseTimesPerWeek.cs
@@ -0,0 +1,13 @@
+﻿namespace FitnessHelper.Enums
+{
+    public enum ExerciseTimesPerWeek
+    {
+        One = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+        Seven = 7
+    }
+}
