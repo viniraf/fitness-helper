95326ac2bdf66c46664f0216b185ec330f7e9882
viniraf <vrmb.pro@gmail.com>
2024-02-22 22:46:55 -0300

    Created endpoint macrodistribution/loseweight

--- FitnessHelper/Program.cs
+++ FitnessHelper/Program.cs
@@ -87,4 +87,42 @@ app.MapGet("/bmr/withexercise", ([FromQuery] Sex sex, double weight, double heig
 .WithTags("Basal Metabolic Rate")
 .WithMetadata(new SwaggerOperationAttribute("Returns Basal Metabolic Rate considering exercise"));
 
+app.MapGet("/macrodistribution/loseweight", (int basalMetabolicRate, double weight) =>
+{
+    double loseWeightBasalMetabolicRate = basalMetabolicRate - 500;
+
+    double totalCalories = loseWeightBasalMetabolicRate;
+
+    // Calculate grams of protein
+    double gramsOfProtein = weight * 1.8;
+
+    // Subtract protein calories from total
+    totalCalories = totalCalories - (gramsOfProtein * 4);
+
+    // Calculate grams of fat
+    double gramsOfFat = weight;
+
+    // Subtract fat calories from total
+    totalCalories = totalCalories - (gramsOfFat * 8);
+
+    // Calculate grams of carb
+    double gramsOfCarb = totalCalories / 4;
+
+    return Results.Ok(
+        new
+        {
+            BMR = $"{basalMetabolicRate} calories",
+            LoseWeightBMR = $"{loseWeightBasalMetabolicRate} calories",
+            ProtGram = $"{gramsOfProtein} g",
+            ProtCal = $"{gramsOfProtein * 4} calories from protein",
+            CarbGram = $"{gramsOfCarb} g",
+            CarbCal = $"{gramsOfCarb * 4} calories from carb",
+            FatGram = $"{gramsOfFat} g",
+            FatCal = $"{gramsOfFat * 8} calories from fat",
+        });
+
+})
+.WithTags("Macronutrient Distribution")
+.WithMetadata(new SwaggerOperationAttribute("Returns macronutrient distribution for lose weight"));
+
 app.Run();
