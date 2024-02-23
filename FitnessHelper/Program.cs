ea3b741c769f95f335e296c3b1ea2887f64bf775
viniraf <vrmb.pro@gmail.com>
2024-02-22 22:52:18 -0300

    Created endpoint macrodistribution/maintainweight

--- FitnessHelper/Program.cs
+++ FitnessHelper/Program.cs
@@ -162,4 +162,41 @@ app.MapGet("/macrodistribution/gainweight", (int basalMetabolicRate, double weig
 .WithTags("Macronutrient Distribution")
 .WithMetadata(new SwaggerOperationAttribute("Returns macronutrient distribution for gain weight"));
 
+app.MapGet("/macrodistribution/maintainweight", (int basalMetabolicRate, double weight) =>
+{
+    double maintainWeightBasalMetabolicRate = basalMetabolicRate;
+
+    double totalCalories = maintainWeightBasalMetabolicRate;
+
+    // Calculate grams of protein
+    double gramsOfProtein = weight * 1.8;
+
+    // Subtract protein calories from total
+    totalCalories = totalCalories - (gramsOfProtein * 4);
+
+    // Calculate grams of fat
+    double gramsOfFat = weight;
+
+    // Subtract fat calories from total
+    totalCalories = totalCalories - (gramsOfFat * 8);
+
+    // Calculate grams of carb
+    double gramsOfCarb = totalCalories / 4;
+
+    return Results.Ok(
+        new
+        {
+            BMR = $"{basalMetabolicRate} calories",
+            MaintainWeightBMR = $"{maintainWeightBasalMetabolicRate} calories",
+            ProtGram = $"{gramsOfProtein} g",
+            ProtCal = $"{gramsOfProtein * 4} calories from protein",
+            CarbGram = $"{gramsOfCarb} g",
+            CarbCal = $"{gramsOfCarb * 4} calories from carb",
+            FatGram = $"{gramsOfFat} g",
+            FatCal = $"{gramsOfFat * 8} calories from fat",
+        });
+})
+.WithTags("Macronutrient Distribution")
+.WithMetadata(new SwaggerOperationAttribute("Returns macronutrient distribution for maintain weight"));
+
 app.Run();
