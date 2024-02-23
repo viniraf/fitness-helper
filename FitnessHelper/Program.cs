819ad08bbecf4b233f5ebc08f54257ca48b0523a
viniraf <vrmb.pro@gmail.com>
2024-02-22 22:44:30 -0300

    Created endpoint bmr/withexercise

--- FitnessHelper/Program.cs
+++ FitnessHelper/Program.cs
@@ -50,4 +50,41 @@ app.MapGet("/bmr/withoutexercise", ([FromQuery] Sex sex, double weight, double h
 .WithTags("Basal Metabolic Rate")
 .WithMetadata(new SwaggerOperationAttribute("Returns Basal Metabolic Rate without considering exercise"));
 
+app.MapGet("/bmr/withexercise", ([FromQuery] Sex sex, double weight, double height, int age, [FromQuery] ExerciseTimesPerWeek exerciseTimesPerWeek) =>
+{
+
+    double basalMetabolicRate = 0;
+
+    if (sex == Sex.Male)
+    {
+        basalMetabolicRate = (10 * weight) + (6.25 * height) - (5 * age) + 5;
+    }
+    else if (sex == Sex.Female)
+    {
+        basalMetabolicRate = (10 * weight) + (6.25 * height) - (5 * age) + 161;
+    }
+
+    Dictionary<int, double> multiplicativeFactorMap = new Dictionary<int, double>
+    {
+        {0, 1.2},
+        {1, 1.375},
+        {2, 1.375},
+        {3, 1.55},
+        {4, 1.55},
+        {5, 1.55},
+        {6, 1.725},
+        {7, 1.725}
+    };
+
+    double multiplicativeFactor = multiplicativeFactorMap[Convert.ToInt32(exerciseTimesPerWeek)];
+
+    basalMetabolicRate = basalMetabolicRate * multiplicativeFactor;
+
+    int roundedBaseMetabolicRate = (int)Math.Round(basalMetabolicRate, MidpointRounding.AwayFromZero);
+
+    return Results.Ok(new { BasalMetabolicRateWithExercise = $"{roundedBaseMetabolicRate} calories" });
+})
+.WithTags("Basal Metabolic Rate")
+.WithMetadata(new SwaggerOperationAttribute("Returns Basal Metabolic Rate considering exercise"));
+
 app.Run();
